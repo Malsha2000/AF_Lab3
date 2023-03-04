41a612c75811f89fc9b9474a25440e9a0041392a
Malsha Jayakody <80629089+Malsha2000@users.noreply.github.com>
2023-03-04 09:55:24 +0530

    Making an HTTP request

--- app.js
+++ app.js
@@ -12,9 +12,24 @@ console.log("Hello World");
 // 	console.log("File saved!");
 // });
 
-const http = require("http");
-http.createServer(function (req, res) {
-	res.writeHead(200, { "Content-Type": "text/html" });
-	res.write("Hello World!");
-	res.end();
-}).listen(8090);
+// const http = require("http");
+// http.createServer(function (req, res) {
+// 	res.writeHead(200, { "Content-Type": "text/html" });
+// 	res.write("Hello World!");
+// 	res.end();
+// }).listen(8090);
+
+const https = require("https");
+https
+	.get("https://jsonplaceholder.typicode.com/posts/1", (resp) => {
+		let data = "";
+		resp.on("data", (chunk) => {
+			data += chunk;
+		});
+		resp.on("end", () => {
+			console.log(JSON.parse(data));
+		});
+	})
+	.on("error", (err) => {
+		console.log("Error: " + err.message);
+	});
