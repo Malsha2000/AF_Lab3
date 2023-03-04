dfddccc5aa67d9da121034481dc0945fbc4a8dc7
Malsha Jayakody <80629089+Malsha2000@users.noreply.github.com>
2023-03-04 09:45:39 +0530

    create a web server

--- app.js
+++ app.js
@@ -6,8 +6,15 @@ console.log("Hello World");
 // 	console.log(data);
 // });
 
-const fs = require("fs");
-fs.writeFile("file.txt", "Hello World!", function (err) {
-	if (err) throw err;
-	console.log("File saved!");
-});
+// const fs = require("fs");
+// fs.writeFile("file.txt", "Hello World!", function (err) {
+// 	if (err) throw err;
+// 	console.log("File saved!");
+// });
+
+const http = require("http");
+http.createServer(function (req, res) {
+	res.writeHead(200, { "Content-Type": "text/html" });
+	res.write("Hello World!");
+	res.end();
+}).listen(8090);
